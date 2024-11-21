aafd999101a699feabc66a4438590ab1ead6882b
pardnchiu <dev@pardn.io>
2024-11-21 10:20:40 +0800

    Initial Commit

---
+++ UI RealityTests/UI_RealityTests.swift
@@ -0,0 +1,17 @@
+//
+//  UI_RealityTests.swift
+//  UI RealityTests
+//
+//  Created by Pardn on 2024/11/21.
+//
+
+import Testing
+@testable import UI_Reality
+
+struct UI_RealityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
